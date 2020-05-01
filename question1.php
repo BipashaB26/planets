6823dbe495fe7b8e149c56e1d7a9d85363cf1fae
BipashaB26 <bipasha.bhattacharjee@gmail.com>
2020-05-01 10:24:40 +0530

    planets

---
+++ question1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>Click on the sun or on one of the planets to watch it closer:</p>
+
+<img src="planets.gif" width="145" height="126" alt="Planets" usemap="#planetmap">
+
+<map name="planetmap">
+  <area shape="rect" coords="0,0,82,126" alt="Sun" href="sun.php">
+  <area shape="circle" coords="90,58,3" alt="Mercury" href="mercur.php">
+  <area shape="circle" coords="124,58,8" alt="Venus" href="venus.php">
+</map>
+</body>
+</html>
